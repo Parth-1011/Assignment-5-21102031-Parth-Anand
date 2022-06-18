27de3d2eb654f8160fae28bc2e065cc86bbe6abb
Parth-1011 <103803443+Parth-1011@users.noreply.github.com>
2022-06-18 21:46:58 +0530

    Create 21102031,assignment 5 output.py

---
+++ 21102031,assignment 5 output.py
@@ -0,0 +1,112 @@
+Enter a string :My name is Parth Anand
+Reversed string is dnanA htraP si eman yM
+Enter the range of numbers: 2 100
+Enter number: 8
+8
+16
+24
+32
+40
+48
+56
+64
+72
+80
+88
+96
+Enter first side of the triangle: 11
+Enter second side of the triangle: 12
+Enter third side of the triangle: 12
+Area of the triangle is 58.65950477117924
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+Enter the no of rows: 7
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+
+Enter the range of numbers: 5 100
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+
+77
+154
+231
+308
+385
+462
+
+Enter 1th number: 22
+Enter 2th number: 23
+Enter 3th number: 33
+Enter 4th number: 44
+Enter 5th number: 55
+Enter 6th number: 66
+Enter 7th number: 77
+Enter 8th number: 88
+Enter 9th number: 99
+Enter 10th number: 100
+[22, 23, 33, 44, 55, 66, 77, 88, 99, 100]
+Positive numbers are: 
+22
+23
+33
+44
+55
+66
+77
+88
+99
+100
+Negative numbers are: 
+Odd numbers are: 
+23
+33
+55
+77
+99
+Even numbers are: 
+22
+44
+66
+88
+100
+No of times each number occurs in the List:
+{22: 1, 23: 1, 33: 1, 44: 1, 55: 1, 66: 1, 77: 1, 88: 1, 99: 1, 100: 1}
+
+Enter a string: PEC is a great college
+{'PEC': 1, 'is': 1, 'a': 1, 'great': 1, 'college': 1}
